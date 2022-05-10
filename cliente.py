d1ef0e479a7f0867e1f195a0e8fe1497eb105cef
ThePernalonga <62114761+ThePernalonga@users.noreply.github.com>
2022-05-10 11:22:53 -0300

    Att Comentarios

--- cliente.py
+++ cliente.py
@@ -5,58 +5,70 @@
 #
 # Andre L. G. Santos 2090279
 
-# Pyro4 - comunicacao entre objetos de um cliente e servidor
-# uuid - gerador de identificadores univocos
-
+from hashlib import sha256
+from Crypto.Signature.pkcs1_15 import PKCS115_SigScheme
 from threading import Thread
+import sys
 import Pyro4
 import uuid
 
 OBJ = "Servidor" # Nome do objeto que deseja conectar
-  
+
+# Critoreios para Conexao com o Servidor
 URI = Pyro4.locateNS().lookup(OBJ)
 refConnec = Pyro4.core.Proxy(URI)
 
-SIGN_PUB = ""
-TOKEN = [] # Vazio por enquanto
-PROCID = str(uuid.uuid1())[:8]
-connec = refConnec.msgIni(PROCID)
 
+SIGN_PUB = "" # Assinatura publica do servidor
+PROCID = str(uuid.uuid1())[:8] # Identificador do processo
+connec = refConnec.msgIni(PROCID) # Inicializa o processo
+
+VERI = []
+
+# Identificacao de recurso
 COCA = 1
 PEPSI = 2
 
+# Classe de callback
 class CallbackHandler(object):
   @Pyro4.expose
   @Pyro4.callback
   def call1(self):
     print("Callback recebido do servidor!")
+    
+  @Pyro4.expose
+  @Pyro4.callback
+  def notify(self, msg):
+    print(">Notificando Servidor: " + msg)
   
   def loopThread(daemon):
     daemon.requestLoop()
     
     
-
+# Registra callbacks no servico de nomes
 daemon = Pyro4.core.Daemon()
 callback = CallbackHandler()
 daemon.register(callback)
 
-
+# Classe principal
 class Cliente(object):
   
   print("Bem vindo ao sistema!\nVoce está conectado agora com " + OBJ)
   
-  # print(refConnec.doCallback(callback, "test"))
+  # Inicializa a thread de callbacks
   msg = Thread(target=CallbackHandler.loopThread, args=(daemon, ))
   msg.setDaemon(True)
   msg.start()
+  # Realiza o test do primeiro callback
+  refConnec.doCallback(callback)
 
 
 # Loop principal, checa se foi feita mesmo a conexão
   while True:
-    refConnec.doCallback(callback)
+    # caso aconteca algum erro de conexão ele fecha o programa
     if connec != 1:
       print("Erro ao conectar com o servidor!")
-      break
+      sys.exit()
     
     resp = input("\n1 - Solicitar acesso ao recurso\n2 - Ver se há chave publica\n3 - Liberar o Recurso\n4 - Sair\n\n")
     
@@ -65,32 +77,34 @@ class Cliente(object):
       while True:
         alter = input("\nQual você deseja solicitar?\n1 - Coca-Cola\n2 - Pepsi\n")
         if alter == '1':
-          if SIGN_PUB != "":
+          if SIGN_PUB != "": # Checa se ja possui chave publica
+            # global VERI
             SIGN_PUB = refConnec.get_public_key()
-          print(refConnec.AcessoRecurso(PROCID, COCA))
+            # VERI = PKCS115_SigScheme(SIGN_PUB)
+          refConnec.iniCallbacks1(callback) # Coloca na fila o callback
+          refConnec.AcessoRecurso(PROCID, COCA) # Solicita o recurso
           alter = 0
           break
           
         elif alter == '2':
           if SIGN_PUB != "":
             SIGN_PUB = refConnec.get_public_key()
-          print(refConnec.AcessoRecurso(PROCID, PEPSI))
+          refConnec.iniCallbacks2(callback)
+          refConnec.AcessoRecurso(PROCID, PEPSI)
           alter = 0
           break
         
-          
         else:
           print("\nOpção inválida!")
         
-          
-      
     elif resp == "2":
       if SIGN_PUB != []:
         print(SIGN_PUB)
       else:
         print("Não há chave ainda!")
+        
     elif resp == '3':
-      if refConnec.isClientWithRec(PROCID):
+      if refConnec.isClientWithRec(PROCID): # Checa se o cliente esta com o recurso
         while True:
           alter = input("\nQual você deseja liberar?\n1 - Coca-Cola\n2 - Pepsi\n")
           if alter == '1':
@@ -106,9 +120,10 @@ class Cliente(object):
 
       else:
         print("Não é possível liberar o recurso, pois não é o dono!")
+        
     elif resp == "4":
       refConnec.removeClient(PROCID)
-      break
+      sys.exit()
     else:
       print("Comando inválido!")
 
--- servidor.py
+++ servidor.py
@@ -5,53 +5,72 @@
 #
 # Andre L. G. Santos 2090279
 
-# Pyro4 - comunicacao entre objetos de um cliente e servidor
-
 import sys
-
 import Pyro4
 import Crypto.Hash.SHA256
 import Crypto.PublicKey.RSA
-import Crypto.Random
+from Crypto.Signature.pkcs1_15 import PKCS115_SigScheme as PKCS115
+from Crypto.Hash import SHA256
 from threading import Thread, Lock, Event
 
-TOKEN = ["Coca-cola", "Pepsi"]
+# Identificacao de chaves
+SIGNER = PKCS115
 SIGN_PUB = []
 SIGN_PRI = []
 
+# Tempo maximo de acesso ao recurso
 TEMP_MAX = 10
 
+# Identificacao de recurso
 COCA = 1
 PEPSI = 2
 
+# Identificadores para fila de processos
 IDENTIFIER1 = ""
 IDENTIFIER2 = ""
 REC1_FREE = True
 REC2_FREE = True
-
 FILAREC1 = []
 FILAREC2 = []
+CALLBACKS1 = []
+CALLBACKS2 = []
 
+# Lista dos processos conectados
 PROC_NAME_LIST = []
 
 @Pyro4.behavior(instance_mode="single")
 class Servidor(object):
   @Pyro4.expose  
-  def doCallback(self, callback):
+  def doCallback(self, callback): # Callback inicial
     print(">Realizando callback no client")
+    self.notify = callback
     return callback.call1()
-    
   
+  # Fila de callbacks
+  @Pyro4.expose  
+  def iniCallbacks1(self, callback):
+    if(len(FILAREC1) > 0):
+      CALLBACKS1.append(callback)
+  
+  # Fila de callbacks
+  @Pyro4.expose  
+  def iniCallbacks2(self, callback):
+    if(len(FILAREC2) > 0):
+      CALLBACKS2.append(callback)
+  
+  notify = {} # Objeto de comunicacao callback
+  # Eventos de wait e lock para os recursos
   lock1 = Lock()
   lock2 = Lock()
   exit1 = Event()
   exit2 = Event()
   
+  # Retorna se o cliente tem o recurso
   @Pyro4.expose
   def isClientWithRec(self, procid):
-    self.doCallback(self.callback, "test")
     return procid == IDENTIFIER1
   
+  # Funcao para liberar reursos
   @Pyro4.expose
   def freeRec(self, rec):
     if rec == COCA:
@@ -61,103 +80,111 @@ class Servidor(object):
     else:
       return -1
   
+  # Fila de processos do recurso 1
   def task1(self, lock, identifier, value):
     global REC1_FREE, IDENTIFIER1
     with lock:
       REC1_FREE = False
-      # print(FILAREC1)
       if len(FILAREC1) == 0:
         IDENTIFIER1 = ""
         REC1_FREE = True
-        # print(REC1_FREE)
-        print(f'>Processo {identifier} liberou! COCA')
+        print(f'>Processo {IDENTIFIER1} liberou! COCA')
       else:
         self.exit1.clear()
         IDENTIFIER1 = FILAREC1[0]
-        print(f'>Processo {identifier} está segurando por {value} segundos')
+        print(f'>Processo {IDENTIFIER1} está segurando por {value} segundos')
         while not self.exit1.is_set():
           self.exit1.wait(value)
           self.exit1.set()
         FILAREC1.pop(0)
+        if len(CALLBACKS1) > 0 and len(FILAREC1) > 0:
+          CALLBACKS1[1].notify("Recurso Livre!")
+        CALLBACKS1.pop(0)
         REC1_FREE = True
         self.lock1 = Lock()
         self.task1(self.lock1, IDENTIFIER1, value)
   
+  # Fila de processos do recurso 2
   def task2(self, lock, identifier, value):
     global REC2_FREE, IDENTIFIER2
     with lock:
       REC2_FREE = False
-      # print(FILAREC2)
       if len(FILAREC2) == 0:
         IDENTIFIER2 = ""
         REC2_FREE = True
-        # print(REC2_FREE)
-        print(f'>Processo {identifier} liberou! PEPSI')
+        print(f'>Processo {IDENTIFIER2} liberou! PEPSI')
       else:
         self.exit2.clear()
         IDENTIFIER2 = FILAREC2[0]
-        print(f'>Processo {identifier} está segurando por {value} segundos')
+        print(f'>Processo {IDENTIFIER2} está segurando por {value} segundos')
         while not self.exit2.is_set():
           self.exit2.wait(value)
           self.exit2.set()
         FILAREC2.pop(0)
+        if len(CALLBACKS2) > 0 and len(FILAREC2) > 0:
+          CALLBACKS2[1].notify("Recurso Livre!")
+        CALLBACKS2.pop(0)
         REC2_FREE = True
         self.lock2 = Lock()
         self.task2(self.lock2, IDENTIFIER2, value)
         
+  # Funcao para adicionar processos e checar primeira conexao
   @Pyro4.expose
   def msgIni(self, name):
     print("Sua conexão foi estabelecida com sucesso! ID: " + name)
     PROC_NAME_LIST.append(name)
     return 1
 
+  # Funcao para remover processos
   @Pyro4.expose
   def removeClient(self, name):
     print("Cliente " + name + " foi desconectado!")
     PROC_NAME_LIST.remove(name)
     return 1
 
+  # Funcao para dar acesso aos recursos
   @Pyro4.expose
   def AcessoRecurso(self, name, tipo: int):
     global REC1_FREE, REC2_FREE
     if tipo == COCA:
       global FILAREC1
       if REC1_FREE == True:
-        # print(REC1_FREE)
         FILAREC1.append(name)
+        CALLBACKS1.append(self.notify)
         Thread(target = self.task1, args=(self.lock1, name, TEMP_MAX)).start()
       
       elif REC1_FREE == False:
         FILAREC1.append(name)
-        return "Recurso não disponível, entrando na fila..."
-      
-      return f"Recurso {tipo} alocado para {name}"
+        return 1
+      return 1
     
     if tipo == PEPSI:
       global FILAREC2
       if REC2_FREE == True:
-        # print(REC2_FREE)
         FILAREC2.append(name)
+        CALLBACKS2.append(self.notify)
         Thread(target = self.task2, args=(self.lock2, name, TEMP_MAX)).start()
       
       elif REC2_FREE == False:
         FILAREC2.append(name)
-        return "Recurso não disponível, entrando na fila..."
-      
-      return f"Recurso {tipo} alocado para {name}"
-
-  def chavesPP():
-    print("Gerando chaves...")
-    SIGN_PUB = Crypto.PublicKey.RSA.generate(1024)
-    SIGN_PRI = SIGN_PUB.exportKey()
-    SIGN_PUB = SIGN_PUB.publickey().exportKey()
-    return 1
-
+        return 1
+      return 1
+    
+  # Funcao para enviar a chave publica
   @Pyro4.expose
   def get_public_key(self):
     return SIGN_PUB
 
+  # Funcao que gera chaves
+  def chavesPP():
+    print("Gerando chaves...")
+    SIGN_PRI = Crypto.PublicKey.RSA.generate(1024)
+    SIGN_PUB = SIGN_PRI.publickey()
+    SIGNER = PKCS115(SIGN_PRI)
+    return 1
+
   print("Servidor iniciado!")
+  # Caso eles nao consigam gerar chaves ele fecha o servidor
   try:
     inicio = chavesPP()
     if inicio == 1:
@@ -173,8 +200,7 @@ class Servidor(object):
       sys.exit()
       
   
-    
-
+  # Inicia o servidor
 daemon = Pyro4.Daemon()
 uri = daemon.register(Servidor)
 print(uri)
